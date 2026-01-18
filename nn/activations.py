18d4b355176960711c54b8fd5482f66c627dc4db
rajatmohan <rajatm2196@gmail.com>
2026-01-18 19:36:35 +0530

    Return a single value from forward function if list length is only one

--- nn/activations.py
+++ nn/activations.py
@@ -3,13 +3,13 @@ from nn.module import Module
 class ReLU(Module):
     def forward(self, x):
         if isinstance(x, list):
-            return [xi.relu() for xi in x]
+            return x[0].relu() if len(x) == 1 else [xi.relu() for xi in x]
         return x.relu()
 
 class Tanh(Module):
     def forward(self, x):
         if isinstance(x, list):
-            return [xi.tanh() for xi in x]
+            return x[0].tanh() if len(x) == 1 else [xi.tanh() for xi in x]
         return x.tanh()
 
 
